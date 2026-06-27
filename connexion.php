78850cb29b69fe524c68e8c3d953a9a62b608588
SalmaGH1 <ghezielsalma25@gmail.com>
2026-06-28 01:51:20 +0200

    connexion

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=GreenMarket', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;port=3307;dbname=greenmarket', 'root', '');
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
     die("Erreur : " . $e->getMessage());
